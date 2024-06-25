abaef2ab075e8f1728d9d4f9b495f8619391bc04
JanaEl-khatib <111792544+JanaEl-khatib@users.noreply.github.com>
2024-06-24 21:55:51 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,29 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    const { username, password } = req.body;
+
+    if (username, password) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            username // You can include additional data in the token payload if needed
+        }, 'your_jwt_secret', { expiresIn: '1h' });
+
+        // Store access token and username in session
+        req.session.username = username;
+
+        return res.status(200).json({ message: "The review has been deleted!"});
+    } else {
+        return res.status(401).json({ message: "Invalid login credentials" });
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
